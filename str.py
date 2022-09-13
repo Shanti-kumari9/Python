85509676e9f7ab4e635b9c5a16bbea280ebefdda
Girl Icon Shanti <91754626+Shanti-kumari9@users.noreply.github.com>
2022-09-13 23:08:52 +0530

    Add files via upload

--- str.py
+++ str.py
@@ -1,14 +1,26 @@
-# str1 = "Welcome to my Blog"
-# for i in str1:
-#     print(i)
-#     print(i, end =' ')
-# print(i, end = '#')
+# str="My Blog"
+# a=" "
+# for i in range(len(str)):
+#     a+=str[i]
+#     print(a)
 
-# ========================================================================================
+# str='MyBLog'                    
+# a=' '
+# for i in range(len(str)):
+#     print(i * str[i])
 
-# a="Shanti"
-# for i in a:
-#     print(i)
-#     print(i,end="")
-# print(i,end="")
+
+# s='My'                        
+# s1='Blog'                        
+# s2=s[:1]+s1[len(s1)-1:]                        
+# print(s2)
+
+# for i in range(2,7,2):                
+#     print(i * '2')
+
+
+
+    
+
+
 
